5698012d248b3868c01a8eb915ca6ae5418cf0e3
HZM111368024 <t111368024@ntut.org.tw>
2023-01-04 21:38:40 +0800

    Final commit

--- Golomb.py
+++ Golomb.py
@@ -13,8 +13,8 @@ def golomb_coding(n: int, m: int) -> str:
     b: int
         Module.
     """
-    #print("n and m : " + str(n) + "  " + str(m))
-    return inverted_unary(n // (m ** 2)) + minimal_binary_coding(n % int((m ** 2)), int(m ** 2))
+
+    return inverted_unary(n // int((2 ** m))) + minimal_binary_coding(n % int((2 ** m)), int(2 ** m))
 
 
 def golomb_decoding(golomb_code: str, b: int) -> str:
@@ -26,13 +26,14 @@ def golomb_decoding(golomb_code: str, b: int) -> str:
     b: int
         Module.
     """
-    inverted_unary_portion_len = get_len_of_leading_inverted_unary(golomb_code)
-    decoded_inverted_unary_portion = decode_leading_inverted_unary(golomb_code)
+    unary_portion_len = get_len_of_leading_unary(golomb_code)
+    decoded_unary_portion = decode_leading_unary(golomb_code)
     decoded_minimal_binary = decode_minimal_binary(
-        golomb_code[inverted_unary_portion_len:],
+        golomb_code[unary_portion_len:],
         b
     )
-    return decoded_inverted_unary_portion * b + decoded_minimal_binary
+
+    return decoded_unary_portion * b + decoded_minimal_binary
 
 
 def best_golomb_coding(numbers: List[int]) -> List[str]:
@@ -46,21 +47,55 @@ def best_golomb_coding(numbers: List[int]) -> List[str]:
     N = len(numbers)
     for n in numbers:
         frequencies[n] = frequencies.get(n, 0) + 1
-
     k = 0
     for n in numbers:
-        #print(frequencies[n] / N, n)
         if frequencies[n] / N == 1 and (n == 0 or n == 1):
             k = 1
         elif frequencies[n] / N == 1:
-            k = frequencies[n] / N * ceil(log2(n))
+            k = 1 / N * int(round(log2(n), 0))
             break
         else:
-            k += frequencies[n] / N * ceil(log2(n))
-
+            if n == 0:
+                k += 1 / N * 0
+            elif n == 1:
+                k += 1 / N * 1
+            else:
+                k += 1 / N * int(round(log2(n), 0))
     return [
         golomb_coding(n, k)
         for n in numbers
+    ]
+
+
+def best_golomb_m(numbers: List[int]) -> List[str]:
+    """Return list of strings representing given numbers in bernoulli golomb coding.
+    Parameters
+    ------------------------
+    numbers: List[int]
+        List of numbers to convert to bernoulli golomb coding.
+    """
+    frequencies = {}
+    N = len(numbers)
+    for n in numbers:
+        frequencies[n] = frequencies.get(n, 0) + 1
+
+    k = 0
+    for n in numbers:
+        # print(frequencies[n] / N, n)
+        if frequencies[n] / N == 1 and (n == 0 or n == 1):
+            k = 1
+        elif frequencies[n] / N == 1:
+            k = 1 / N * int(round(log2(n), 0))
+            break
+        else:
+            if n == 0:
+                k += 1 / N * 0
+            elif n == 1:
+                k += 1 / N * 1
+            else:
+                k += 1 / N * int(round(log2(n), 0))
+    return [
+        k, int((2 ** k))
     ]
 
 
@@ -70,7 +105,8 @@ def get_maximal_code_length(b: int) -> int:
 
 
 def minimal_binary_coding(n: int, b: int) -> str:
-    """Return string representing given number in minimal binary coding.
+    # print(b)
+    """Return string representing given number in minimal binary coding.`
 
     Parameters
     -------------------------
@@ -79,12 +115,13 @@ def minimal_binary_coding(n: int, b: int) -> str:
     b: int
         Maximal size.
     """
-    if n == 0 and b == 1:
-        return "0"
     code_maximal_len = get_maximal_code_length(b)
-    if n < 2 ** code_maximal_len - b:
+    if code_maximal_len == 0:
+        return ''
+    elif n < 2 ** code_maximal_len - b:
         return f"{{0:0{code_maximal_len - 1}b}}".format(n)
-    return f"{{0:0{code_maximal_len}b}}".format(n - b + 2 ** code_maximal_len)
+    else :
+        return f"{{0:0{code_maximal_len}b}}".format(n - b + 2 ** code_maximal_len)
 
 
 def decode_minimal_binary(minimal_binary_code: str, b: int) -> int:
@@ -110,17 +147,18 @@ def _unary(n: int, zero: str, one: str) -> str:
 
 
 def inverted_unary(n: int) -> str:
+    # print(n)
     """Return string representing given number in inverted unary.
         n:int, number to convert to unary.
     """
     return _unary(n, "0", "1")
 
 
-def get_len_of_leading_inverted_unary(string_starting_with_inverted_unary: str) -> int:
+def get_len_of_leading_unary(string_starting_with_unary: str) -> int:
     """Returns length of leading inverted unary portion in string."""
-    return decode_leading_inverted_unary(string_starting_with_inverted_unary) + 1
+    return decode_leading_unary(string_starting_with_unary) + 1
 
 
-def decode_leading_inverted_unary(string_starting_with_inverted_unary: str) -> int:
+def decode_leading_unary(string_starting_with_unary: str) -> int:
     """Returns decoded unary value."""
-    return string_starting_with_inverted_unary.index("1")
+    return string_starting_with_unary.index("0")
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-from Golomb import golomb_coding, golomb_decoding, best_golomb_coding
+from Golomb import golomb_coding, golomb_decoding, best_golomb_coding, best_golomb_m
+from math import ceil, log2
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+import numpy as np
 
-# print(golomb_coding(0, 3))
-# print(golomb_coding(1, 3))# 110
-# print(golomb_coding(2, 3)) # 111
-#print(golomb_coding(5, 2))
-matrix = [0, 0, 0, 0, 0]
-matrix2 = [1, 1, 1, 1, 1]
-matrix3 = [115, 115, 115, 115, 115]
-matrix_len = 0
+#matrix = [0, 0, 0, 0, 1, 1, 1, 1]
+#matrix = [0, 0, 0, 0, 1, 1, 1, 1]
+matrix = np.random.randint(0,100,10)
 
-print("Code : " + str(best_golomb_coding(matrix)))
-print("Code : " + str(best_golomb_coding(matrix2)))
-print("Code : " + str(best_golomb_coding(matrix3)))
+diff_m = []
+diff_k = []
+diff_m_matrix_len = []
+print(matrix)
+print("Best (k,m) : " + str(best_golomb_m(matrix)))
+k = ((best_golomb_m(matrix))[0])
+for i in range(-10, 10, 1):
+    matrix_len = 0
+    if k + i > 0:
+        for j in range(len(matrix)):
+            matrix_len += len((golomb_coding(matrix[j], k+ i)))
+        diff_k.append(round(k + i, 2))
+        diff_m.append(int(2**(k+i)))
+        diff_m_matrix_len.append(matrix_len)
 
-for i in range(len(matrix)):
-    matrix_len +=len(best_golomb_coding(matrix)[i])
+print(golomb_decoding("100",3))
+print("Different k : " + str(diff_k))
+print("Different m : " + str(diff_m))
+print("Different matrix length : " + str(diff_m_matrix_len))
+print("Best golomb coding : " + str(best_golomb_coding(matrix)))
+fig = plt.figure()
+plt.title("Best golom coding parameter m")
+plt.xlabel("m")
+plt.ylabel("codeword length")
 
-print("Total code length : " + str(matrix_len))
-print("Average code length : " +str(matrix_len / len(matrix3)))
+for i in range(len(diff_m)):
+    plt.text(diff_m[i] + 0.25, diff_m_matrix_len[i] - 0.5, (round(diff_m[i], 2), diff_m_matrix_len[i]))
+plt.plot(diff_m, diff_m_matrix_len, marker='o', mec='r', mfc='w')
+plt.show()
